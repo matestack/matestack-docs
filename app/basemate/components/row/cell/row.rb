aa7f9c3815938ab08fe30f441f7e32e49f1cc5c6
Jonas Jabari <jonas.jabari@gmx.net>
2018-10-08 16:54:21 +0200

    added row and col as custom component as they were moved out of core, removed row col customizers

---
+++ app/basemate/components/row/cell/row.rb
@@ -0,0 +1,17 @@
+module Components
+  module Row::Cell
+    class Row < Component::Cell::Static
+
+      def row_classes
+        classes = []
+
+        classes << "row"
+        classes << "mdl-grid"
+        classes << "mdl-grid--no-spacing" if options[:no_spacing]
+
+        return classes.join(" ")
+      end
+
+    end
+  end
+end
