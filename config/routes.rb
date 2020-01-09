67797c64dc675f661c946a6d05d0fe6d032db0b9
Jonas Jabari <jonas@basemate.com>
2020-01-09 17:49:00 +0100

    WIP reduce to docs, moving landingpage to different repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: 'landingpage#home'
-
-  get 'bangkok_challenge', to: 'landingpage#rubyconfth'
-  get 'imprint', to: 'landingpage#imprint'
-  get 'privacy', to: 'landingpage#privacy'
+  root to: redirect('/docs/install')
 
   scope :docs do
     get '/components/*key', to: 'components#resolve', as: "components"
