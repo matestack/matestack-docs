ffb90f3358353e4284aa762f122c968be388e2c2
Jonas Jabari <jonas@basemate.com>
2020-01-29 12:31:46 +0100

    fix github content url due to github orga renaming

--- app/matestack/pages/docs/github_doc.rb
+++ app/matestack/pages/docs/github_doc.rb
@@ -2,7 +2,7 @@ class Pages::Docs::GithubDoc < Pages::Base::GithubDoc
 
   def prepare
     super
-    @github_api_md_path = "https://raw.githubusercontent.com/basemate/matestack-ui-core/#{@branch}/docs/#{@file}"
+    @github_api_md_path = "https://raw.githubusercontent.com/matestack/matestack-ui-core/#{@branch}/docs/#{@file}"
     @sub_title = "Documentation"
   end
 
