9d6282354598036e45fb203071d9130a8cb43c5b
Nils Henning <nils@matestack.io>
2020-09-07 15:17:36 +0200

    [BUGFIX] content overflowing header on smaller screens

--- app/matestack/docs/pages/base.rb
+++ app/matestack/docs/pages/base.rb
@@ -26,7 +26,7 @@ class Docs::Pages::Base < Matestack::Ui::Page
   end
 
   def hero
-    section id: 'hero', class: 'pt-5 pb-2 m-5 rounded' do
+    section id: 'hero', class: 'pt-5 pb-2 my-5 rounded' do
       div class: 'container' do
         div class: 'row py-3' do
           div class: 'col-md-4 offset-md-3 col-12' do
@@ -40,8 +40,8 @@ class Docs::Pages::Base < Matestack::Ui::Page
 
   def content
     section id: 'content' do
-      div class: 'container m-5' do
-        div class: 'row py-4' do
+      div class: 'container' do
+        div class: 'row py-4 px-5' do
           div class: 'col-12' do
            docs_md path: @github_api_md_path, remote: true, lang: @md_language_wrapper
           end
